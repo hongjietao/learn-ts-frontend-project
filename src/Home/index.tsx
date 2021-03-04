578a2f3b577c33b905b2ef1933985efa2a6c848a
陶鸿杰 <taohongjie@bytedance.com>
2021-03-04 16:34:11 +0800

    install echarts

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { Button, message } from "antd";
 import axios from "axios";
+import echarts from "echarts";
+import ReactECharts from "echarts-for-react";
 import "./style.css";
 
 class Home extends Component {
@@ -38,19 +40,84 @@ class Home extends Component {
 
   handleCrowllerClick = () => {};
 
+  getOption: () => echarts.EChartsOption = () => {
+    return {
+      title: {
+        text: "折线图堆叠",
+      },
+      tooltip: {
+        trigger: "axis",
+      },
+      legend: {
+        data: ["邮件营销", "联盟广告", "视频广告", "直接访问", "搜索引擎"],
+      },
+      grid: {
+        left: "3%",
+        right: "4%",
+        bottom: "3%",
+        containLabel: true,
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {},
+        },
+      },
+      xAxis: {
+        type: "category",
+        boundaryGap: false,
+        data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          name: "邮件营销",
+          type: "line",
+          stack: "总量",
+          data: [120, 132, 101, 134, 90, 230, 210],
+        },
+        {
+          name: "联盟广告",
+          type: "line",
+          stack: "总量",
+          data: [220, 182, 191, 234, 290, 330, 310],
+        },
+        {
+          name: "视频广告",
+          type: "line",
+          stack: "总量",
+          data: [150, 232, 201, 154, 190, 330, 410],
+        },
+        {
+          name: "直接访问",
+          type: "line",
+          stack: "总量",
+          data: [320, 332, 301, 334, 390, 330, 320],
+        },
+        {
+          name: "搜索引擎",
+          type: "line",
+          stack: "总量",
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+        },
+      ],
+    };
+  };
+
   render() {
     const { isLogin, loaded } = this.state;
     if (isLogin) {
       if (loaded) {
         return (
           <div className="home-page">
-            <Button type="primary" onClick={this.handleCrowllerClick}>
-              爬取
-            </Button>
-            <Button type="primary">show</Button>
-            <Button type="primary" onClick={this.handleLogoutClick}>
-              退出
-            </Button>
+            <ReactECharts option={this.getOption()} />
+            <div className="buttons">
+              <Button type="primary" onClick={this.handleCrowllerClick}>
+                爬取
+              </Button>
+              <Button onClick={this.handleLogoutClick}>退出</Button>
+            </div>
           </div>
         );
       } else {
